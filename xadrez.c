bc256b49b8db2166a7df5e371fc0b2cb2faca3c9
yrodrigues-dev <yrodrigues.dev@gmail.com>
2025-09-14 22:52:53 +0000

    Adicionei a movimentação do cavalo

--- xadrez.c
+++ xadrez.c
@@ -25,9 +25,25 @@ int main() {
         {
             printf("Esquerda\n"); r++;
         } while (r <= 8);
-        
 
+    // ==================== CAVALO ====================
+    
+    int passos1 = 2;
+    int passos2 = 1;
+    printf("\nMovimento do Cavalo:\n");
+
+        for(int x = 1; x <= passos1; x++) {
+            printf("Baixo\n");
+
+        }
+
+    int y = 1;
+        while (y <= passos2) {
+            printf("Esquerda\n");
+            y++;
+
+        }
 
 return 0;
-
 }
+        
